e9432782c017410c19bbd3bb008160d70bcf3072
Daniel-OBatista <daniel.celapp@gmail.com>
2025-07-13 00:06:25 -0300

    Projeto - versão2

--- src/app/admin/servicos/page.tsx
+++ src/app/admin/servicos/page.tsx
@@ -5,7 +5,6 @@ import { supabase } from '../../lib/supabaseClient'
 import { useRouter } from 'next/navigation'
 import { Home } from 'lucide-react'
 import ImageCropper from '../../../../utils/ImageCropper'
-import Image from 'next/image'
 
 export default function ServicosAdminPage() {
   type Servico = {
@@ -309,13 +308,11 @@ export default function ServicosAdminPage() {
         {servicosFiltrados.map((s) => (
           <div key={s.id} className="bg-white p-3 rounded-md shadow-sm border border-pink-100">
             {s.imagem_url && (
-              <Image
-              src={s.imagem_url}
-              alt={s.nome}
-              width={320}
-              height={130}
-              className="rounded mb-2 mx-auto bg-zinc-100 object-contain"
-            />
+              <img
+                src={s.imagem_url}
+                alt={s.nome}
+                className="w-[320px] h-[130px] object-contain rounded mb-2 mx-auto bg-zinc-100"
+              />
             )}
             <p className="font-semibold text-pink-600">{s.nome}</p>
             <p className="text-xs text-zinc-600">{s.descricao}</p>
